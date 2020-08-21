4e6b4aef8d96691385a0987d28618c86ff02f352
Wei Chuan Beh <weichuan@Alienware-m17.local>
2020-08-22 02:14:07 +0800

    change swagger path

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,7 @@ async function bootstrap() {
   const appDocument = SwaggerModule.createDocument(app, options, {
     include: [EPaymentModule]
   });
-<<<<<<< HEAD
   SwaggerModule.setup('swagger', app, appDocument);
-=======
-  SwaggerModule.setup('/ipay88/api', app, appDocument);
->>>>>>> f590f7291a364ff12f2b93132b26a58ee76dae0f
 
   const port = process.env.port || 9001;
   await app.listen(port);
