110137f9de9af3a1e37744ce1a06d829e4647df6
Wei Chuan Beh <weichuan@Alienware-m17.local>
2020-08-22 00:52:36 +0800

    change to use port 9001

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,8 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api', app, appDocument);
 
-  const port = process.env.port || 3000;
-  await app.listen(3000);
+  const port = process.env.port || 9001;
+  await app.listen(port);
   logger.log(`application listening on port ${port} ...`)
 }
 bootstrap();
